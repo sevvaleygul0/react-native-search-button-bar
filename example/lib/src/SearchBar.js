281d8753dd7d834e50db8a4f856693c2a2910004
sevval <sevvalleygull@gmail.com>
2020-07-07 22:08:32 +0300

    files edited

---
+++ example/lib/src/SearchBar.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import {View, Text} from 'react-native';
+
+export default class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <View>
+        <Text> SearchBar </Text>
+      </View>
+    );
+  }
+}
